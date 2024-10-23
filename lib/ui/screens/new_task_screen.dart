8efbd22e25630cb196a3cc3e6d9a17576a5c9d26
khandakerahsunhabib <ahsunhabib96bd@gmail.com>
2024-10-23 07:52:25 +0600

    add new task screen added

--- lib/ui/screens/new_task_screen.dart
+++ lib/ui/screens/new_task_screen.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
-class NewTaskScreen extends StatelessWidget {
+import 'package:task_manager/ui/screens/add_new_task_screen.dart';
+
+class NewTaskScreen extends StatefulWidget {
   const NewTaskScreen({super.key});
 
   @override
+  State<NewTaskScreen> createState() => _NewTaskScreenState();
+}
+
+class _NewTaskScreenState extends State<NewTaskScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('New Task Screen'),);
+    return Scaffold(
+      body: const Center(
+        child: Text('New Task Screen'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        shape: const StadiumBorder(),
+        onPressed: _onTapAddFAB,
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+
+  void _onTapAddFAB() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const AddNewTaskScreen(),
+      ),
+    );
   }
 }
